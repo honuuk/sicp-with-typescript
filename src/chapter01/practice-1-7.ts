9d58c3eac1ee1436d8d833c8910b984b8e9f8916
jewook3617 <jewook3617@gmail.com>
2022-11-25 22:51:16 +0900

    practice 1.7 solution

--- src/chapter01/practice-1-7.ts
+++ src/chapter01/practice-1-7.ts
@@ -1,24 +1,17 @@
 import { abs, add, divide, square, subtract } from "../scheme";
 
-export const sqrt = (x: number): number => sqrtIter(-10000, 1, x);
+export const sqrt = (x: number): number => sqrtIter(1, x);
 
-const sqrtIter = (prevGuess: number, guess: number, x: number): number =>
-  isGoodEnough(prevGuess, guess, x)
-    ? guess
-    : sqrtIter(guess, improve(guess, x), x);
+const sqrtIter = (guess: number, x: number): number =>
+  isGoodEnough(guess, x) ? guess : sqrtIter(improve(guess, x), x);
 
 const improve = (guess: number, x: number): number =>
   average(guess, divide(x, guess));
 
 const average = (x: number, y: number): number => divide(add(x, y), 2);
 
-const isGoodEnough = (prevGuess: number, guess: number, x: number) =>
-  abs(
-    subtract(
-      abs(subtract(square(guess), x)),
-      abs(subtract(square(prevGuess), x))
-    )
-  ) < 0.0001;
+const isGoodEnough = (guess: number, x: number) =>
+  abs(subtract(guess, improve(guess, x))) < 0.0001;
 
 console.log(sqrt(9007199254740992));
 console.log(sqrt(0.001));
